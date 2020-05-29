4e4e4641bc5a70ef79c2f1324236380cc951a67a
Daniel C Istrate <daniel.istr8@gmail.com>
2020-05-29 19:08:20 +0300

    Solving Task 1 - Implementing hello_world() function

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
+#Task 1 - Hello world function implementation
 def hello_world():
-    pass
-
-
+    return "Hello, World!"
+"""
+returns "Hello, World!" without printing the function
+"""
 message = hello_world()
 print(message)
