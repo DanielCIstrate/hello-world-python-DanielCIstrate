dc02e8795d0aa761ea535222c18e0bf4f4fa9fe7
Daniel C Istrate <daniel.istr8@gmail.com>
2020-05-29 22:47:24 +0300

    'hello.py' change. Now it contains Task 4&5 solutions

--- hello.py
+++ hello.py
@@ -1,40 +1,20 @@
-#Task 1 - Hello world function implementation
+#Task 4&5: Hello <name> using command line
+# and it works with multiple arguments
 
-def hello_world():
-    return "Hello, World!"
+import sys
 
-
-"""returns "Hello, World!" without printing the function"""
-message = hello_world()
-"""indicates output for task 1"""
-print("1.")
-"""test"""
-print(message)
-print()
-
-
-
-#Task 2 - 'hello(name)' function
 
 def hello(name):
     if name == None or name == "":
         return 'Hello, World!'
-    return 'Hello, ' + name + '!'
+    else: 
+        return 'Hello,' + name + '!'
+    
+lista = (sys.argv)
 
-"""separates second task output from first."""
-print("2.")
+text= ""
 
-""" if you want to try the code yourself:"""
-
-name = input("Insert a name: ")
-print("Output: ", hello(name))
-
-#Task 3 - Create and implement 'print_hello(name)'
-
-def print_hello(name):
-    print(hello(name))
-
-print()
-print("3.")
-
-print_hello("Daniel")
+for nume in lista[1:]:
+    text += " " + nume
+    
+print(hello(text))
